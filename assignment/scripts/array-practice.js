76fa1156279e367c7e30bfe35cd866d030e24ca2
Shong Lee <Shonglee17@gmail.com>
2022-11-07 22:44:13 -0600

    finished all questions

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -98,11 +98,14 @@ console.log(favFoods)
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
-favFoods.splice(1,0, 'and')
-console.log(favFoods)
+let newThing = favFoods[0] + ' and ' + favFoods[1] + ' and ' + favFoods[2]
+console.log(newThing)
+//this looks like I didn't do any computing at all.
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+let myCombo = favFoods.concat(animalArray);
+console.log(myCombo);
